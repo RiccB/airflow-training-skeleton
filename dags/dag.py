2f123ac11c35ef62d5770057f8204b4bfe39133c
Riccardo Bellana <rbellana@bol.com>
2019-02-25 11:55:47 +0100

    no repetition of operator

--- dags/dag.py
+++ dags/dag.py
@@ -15,26 +15,30 @@ print_date = BashOperator(
     task_id="print_exec_date", bash_command="echo {{ execution_date }}", dag=dag
 )
 
-wait_1 = BashOperator(
-    task_id="wait_1",
-    bash_command="sleep 1",
-    dag=dag
-)
+# wait_1 = BashOperator(
+#     task_id="wait_1",
+#     bash_command="sleep 1",
+#     dag=dag
+# )
+#
+# wait_5 = BashOperator(
+#     task_id="wait_5",
+#     bash_command="sleep 5",
+#     dag=dag
+# )
+#
+# wait_10 = BashOperator(
+#     task_id="wait_10",
+#     bash_command="sleep 10",
+#     dag=dag
+# )
 
-wait_5 = BashOperator(
-    task_id="wait_5",
-    bash_command="sleep 5",
-    dag=dag
-)
-
-wait_10 = BashOperator(
-    task_id="wait_10",
-    bash_command="sleep 10",
-    dag=dag
-)
+operators = []
+for i in [1, 5, 10]:
+    operators.append(BashOperator(task_id='wait_{}'.format(i), bash_command='sleep {}'.format(i), dag=dag))
 
 end = DummyOperator(task_id="end", dag=dag)
 
-print_date >> [wait_1, wait_5, wait_10] >> end
+print_date >> operators >> end
 
 

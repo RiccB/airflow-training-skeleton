e2eeb0f270d3b19a78f82e875af9cd856bf37999
Riccardo Bellana <rbellana@bol.com>
2019-02-25 16:50:32 +0100

    Add date

--- dags/dag2.py
+++ dags/dag2.py
@@ -29,6 +29,8 @@ from airflow.models import Variable
 
 
 def send_to_slack_func(**context):
+    lista = context.get('ti').xcom_pull(key='return_value', task_ids='bq_fetch_data')
+    lista = ['date: {{ds}}'] + lista
     operator = SlackAPIPostOperator(
         task_id='slack_api',
         text=str(context.get('ti').xcom_pull(key='return_value', task_ids='bq_fetch_data')),

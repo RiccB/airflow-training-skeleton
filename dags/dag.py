f45161f8561bc7b508a876d8ca2c2da7f627f228
Riccardo Bellana <rbellana@bol.com>
2019-02-25 11:22:05 +0100

    first pipeline

--- dags/dag.py
+++ dags/dag.py
@@ -1,6 +1,7 @@
 import airflow
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
+from airflow.operators.dummy_operator import DummyOperator
 
 dag = DAG(
     dag_id="hello_airflow",
@@ -10,6 +11,30 @@ dag = DAG(
     },
 )
 
-BashOperator(
+print_date = BashOperator(
     task_id="print_exec_date", bash_command="echo {{ execution_date }}", dag=dag
 )
+
+wait_1 = BashOperator(
+    task_id="wait_1",
+    bash_command="sleep 1",
+    dag=dag
+)
+
+wait_5 = BashOperator(
+    task_id="wait_5",
+    bash_command="sleep 5",
+    dag=dag
+)
+
+wait_10 = BashOperator(
+    task_id="wait_10",
+    bash_command="sleep 10",
+    dag=dag
+)
+
+end = DummyOperator(task_id="end", dag=dag)
+
+print_date >> [wait_1, wait_5, wait_10] >> end
+
+
